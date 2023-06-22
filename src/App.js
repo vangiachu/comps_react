ed64112a6b7b359dfd0568740c93f624302b64b5
Chu Văn Giá <gia.chuvan@ncc.asia>
2023-06-22 17:24:22 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from './Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<div>
+				<Button success primary rounded outline>
+					Click me!
+				</Button>
+			</div>
+			<div>
+				<Button danger outline>
+					Buy Now!
+				</Button>
+			</div>
+			<div>
+				<Button warning>See Deal!</Button>
+			</div>
+			<div>
+				<Button secondary outline>
+					Hide Ads!
+				</Button>
+			</div>
+			<div>
+				<Button secondary rounded>
+					Something!
+				</Button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
